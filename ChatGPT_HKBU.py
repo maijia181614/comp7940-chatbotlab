6ebba442381efa9cf042c1c2e545d62dd11893e0
mjq <472822145@qq.com>
2024-03-11 01:14:07 +0800

    lab05-chatbot

--- ChatGPT_HKBU.py
+++ ChatGPT_HKBU.py
@@ -1,21 +1,16 @@
 import configparser
 import requests
+import os
 
 
 class HKBU_ChatGPT():
-    def __init__(self, config_='./config.ini'):
-        if type(config_) == str:
-            self.config = configparser.ConfigParser()
-            self.config.read(config_)
-        elif type(config_) == configparser.ConfigParser:
-            self.config = config_
 
     def submit(self, message):
         conversation = [{"role": "user", "content": message}]
-        url = (self.config['CHATGPT']['BASICURL']) + "/deployments/" + (
-        self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (
-              self.config['CHATGPT']['APIVERSION'])
-        headers = {'Content-Type': 'application/json', 'api-key': (self.config['CHATGPT']['ACCESS_TOKEN'])}
+        url = (os.environ["chatgpt_basic_url"]) + "/deployments/" + (
+        os.environ["chatgpt_model_name"]) + "/chat/completions/?api-version=" + (
+              os.environ["chatgpt_api_version"])
+        headers = {'Content-Type': 'application/json', 'api-key': (os.environ["chatgpt_access_token"])}
         payload = {'messages': conversation}
         response = requests.post(url, json=payload, headers=headers)
         if response.status_code == 200:
--- chatbot.py
+++ chatbot.py
@@ -3,22 +3,23 @@ from telegram import Update
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackContext)
 from ChatGPT_HKBU import HKBU_ChatGPT
 # The messageHandler is used for all message updates
-import configparser
+# import configparser
 import logging
 import redis
+import os
 
 
 global redis1
 def main():
     # Load your token and create an Updater for your Bot
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    # config = configparser.ConfigParser()
+    # config.read('config.ini')
+    updater = Updater(token=(os.environ["telegram_access_token"]), use_context=True)
     dispatcher = updater.dispatcher
 
     global redis1
-    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']),
-                         port=(config['REDIS']['REDISPORT']))
+    redis1 = redis.Redis(host=(os.environ["redis_host"]), password=(os.environ["redis_password"]),
+                         port=(os.environ["redis_port"]))
     # You can set this logging module,
     # so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -31,10 +32,10 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
-
+    dispatcher.add_handler(CommandHandler("hello", hello))
     # dispatcher for chatgpt
     global chatgpt
-    chatgpt = HKBU_ChatGPT(config)
+    chatgpt = HKBU_ChatGPT()
     chatgpt_handler = MessageHandler(Filters.text & (~Filters.command), equiped_chatgpt)
     dispatcher.add_handler(chatgpt_handler)
     # To start the bot:
@@ -42,6 +43,12 @@ def main():
     updater.idle()
 def echo(update, context):
     reply_message = update.message.text.upper()
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
+
+def hello(update, context):
+    reply_message = "Good day, " + update.message.text + "!"
     logging.info("Update: " + str(update))
     logging.info("context: " + str(context))
     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
